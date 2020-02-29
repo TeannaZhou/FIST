9073bf29f1b4b7879c28750ebf44c7a21577c15c
TeannaZhou <zhoudatou2008@126.com>
2020-02-29 21:13:56 +0800

    update

--- main.py
+++ main.py
@@ -160,7 +160,11 @@ def test(arg):
                 cv2.imshow('Test', img)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
+            else:
+                break
+        camera.release()
         cv2.destroyAllWindows()
+
 
 
 if __name__ == '__main__':
